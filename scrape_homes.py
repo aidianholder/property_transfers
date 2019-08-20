e290aee99744a2b08a3f2fbdc695b1e0429927c1
aidianholder <aidianholder@gmail.com>
2019-08-20 09:02:26 -0700

    working

--- scrape_homes.py
+++ scrape_homes.py
@@ -5,6 +5,7 @@ import dateutil.parser
 import argparse
 import datetime
 import json
+import sqlite3
 
 
 class PropertyList(object):
@@ -47,13 +48,19 @@ class PropertyTransfer(object):
         for k in kwargs:
             setattr(self, k, kwargs[k])
         self.SalePrice = int(self.SalePrice)
-        p_data: Callable[[], Dict[str, Any]] = self.parcel_lookup
+        p_data = self.parcel_lookup
         self.Address = p_data["Address"]
         self.Buyer = p_data["Owner"]
         self.City = p_data["City"]
+        self.ZipCode = p_data['ZipCode']
         self.Seller = self.excise_lookup()
         self.ExciseDate = self.format_excise_date()
         self.ExciseDateString = self.ExciseDate.strftime("%m/%d/%Y")
+        self.transaction_id = int(str(self.ExciseDate.toordinal()) + self.ParcelNumber[:6] + self.ParcelNumber[7:])
+        geographic_coordinates = self.get_coordinates()
+        self.latitude = geographic_coordinates['latitude']
+        self.longitude = geographic_coordinates['longitude']
+
 
     @property
     def parcel_lookup(self):
@@ -84,6 +91,39 @@ class PropertyTransfer(object):
         excise_date_iso = self.ExciseDate
         dt = dateutil.parser.parse(excise_date_iso)
         return dt
+
+    def get_coordinates(self):
+        conn = sqlite3.connect("transfers.db")
+        cur = conn.cursor()
+        parcel_num = int(self.ParcelNumber[:6] + self.ParcelNumber[7:])
+        cur.execute("SELECT LONGITUDE, LATITUDE FROM parcel_centroids WHERE ASSESSOR_N=?", (parcel_num,))
+        cords = cur.fetchone()
+        try:
+            return dict(longitude=cords[0], latitude=cords[1])
+        except TypeError:
+            return dict(longitude=0, latitude=0)
+
+    def load_data(self):
+        conn = sqlite3.connect("transfers.db")
+        cur = conn.cursor()
+        sql = '''INSERT INTO transfers(
+                        transaction_id, 
+                        parcel_number, 
+                        address, city, 
+                        zip_code, 
+                        seller, 
+                        buyer, 
+                        sale_price, 
+                        excise_date, 
+                        structure_type
+                        ) 
+                    VALUES(?,?,?,?,?,?,?,?,?,?)'''
+        property_values = (self.transaction_id, self.ParcelNumber, self.Address, self.City, self.ZipCode)
+        property_values += (self.Seller, self.Buyer, self.SalePrice, self.ExciseDateString, self.StructureType)
+        try:
+            cur.execute(sql, property_values)
+        except sqlite3.IntegrityError:
+            print(self.Address + " already transferred on " + self.ExciseDateString)
 
     def __str__(self):
         base = "{0}; ${1:,}; {2}; {3}; {4}".format(self.Address, self.SalePrice, self.Buyer, self.Seller, self.ExciseDateString)
@@ -135,32 +175,31 @@ class OldTransfers(object):
         json.dumps(self.transfers, sort_keys=True, indent=4)
 
 
-def run_residential(start, end, old):
-    city_groups = {}
-    outfile = open('residential.txt', 'w')
+def run_residential(start, end):
+    # city_groups = {}
+    # outfile = open('residential.txt', 'w')
     res_transfers = PropertyList(start=start, end=end, use="11")
     res_transfers.populate_list()
-    pr = OldTransfers(old)
     for home in res_transfers.list:
         p = PropertyTransfer(**home)
-        pr.record_transfer(p.ExciseDate.strftime("%Y%m%d"), p.ParcelNumber, p.Address)
-        if p.City in city_groups.keys():
-            city_groups[p.City].append(p)
-        else:
-            city_groups[p.City] = []
-            city_groups[p.City].append(p)
-    pr.purge_oldest()
-    pr.write_storage("residential")
-    for town in city_groups.keys():
-        outfile.write(town)
-        outfile.write('\n')
-        city_groups[town].sort(key=lambda x: x.ExciseDate)
-        for q in city_groups[town]:
-            print(q)
-            outfile.write(q.__str__())
-            outfile.write('\n')
-        outfile.write('\n')
-    outfile.close()
+        p.load_data()
+    #    if p.City in city_groups.keys():
+    #        city_groups[p.City].append(p)
+    #    else:
+    #        city_groups[p.City] = []
+    #        city_groups[p.City].append(p)
+    # pr.purge_oldest()
+    # pr.write_storage("residential")
+    # for town in city_groups.keys():
+    #    outfile.write(town)
+    #    outfile.write('\n')
+    #    city_groups[town].sort(key=lambda x: x.ExciseDate)
+    #    for q in city_groups[town]:
+    #        print(q)
+    #        outfile.write(q.__str__())
+    #        outfile.write('\n')
+    #    outfile.write('\n')
+    # outfile.close()
 
 
 def run_commercial(start, end, old):
@@ -168,13 +207,13 @@ def run_commercial(start, end, old):
     com_transfers = PropertyList(start=start, end=end, use=None)
     com_transfers.populate_list()
     com_properties_transferred = []
-    pr = OldTransfers(old)
+    # pr = OldTransfers(old)
     for parcel in com_transfers.list:
         p = PropertyTransfer(**parcel)
-        pr.record_transfer(p.ExciseDate.strftime("%Y%m%d"), p.ParcelNumber, p.Address)
+        # pr.record_transfer(p.ExciseDate.strftime("%Y%m%d"), p.ParcelNumber, p.Address)
         com_properties_transferred.append(p)
-    pr.purge_oldest()
-    pr.write_storage("commercial")
+    # pr.purge_oldest()
+    # pr.write_storage("commercial")
     com_properties_transferred.sort(key=lambda x: x.ExciseDate)
     for v in com_properties_transferred:
         print(v)
@@ -190,5 +229,5 @@ if __name__ == "__main__":
     # parser.add_argument("old")
     # args = parser.parse_args()
     # run_residential(, args.end)
-    run_residential('07/01/2019', '07/15/2019', None)
-    run_commercial('07/01/2019', '07/15/2019', None)
+    run_residential('07/01/2019', '07/05/2019')
+    # run_commercial('07/01/2019', '07/15/2019', None)
